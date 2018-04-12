e1c7ff2239275a8083e8c7905ad646c29b50bf5e
Tristan Canova <tcanova1@gmail.com>
2018-04-11 20:31:56 -0400

    Added css to the table created

--- statueController.js
+++ statueController.js
@@ -4,9 +4,10 @@ var jp = require('jsonpath');
 module.exports = {
     createTable: function(){
         var statues = jp.query(data,'$.*');
-        htmlTable  = '<table>';
+        htmlTable = '<link rel=stylesheet href=/css/style.css><table>';
+        htmlTable += '<tr><th>Title:</th><th>Artist:</th><th>Location:</th><th>Picture:</th></tr>';
         for(i in statues){
-            htmlTable += '<tr><td>'+statues[i].name+'</td>'+'<td>'+statues[i].artist+'</td>'+'<td>'+statues[i].address+'</td>'+'<td><img src ='+statues[i].path+' height=100px width = 100px></td></tr>';
+            htmlTable += '<tr><td>'+statues[i].name+'</td>'+'<td>'+statues[i].artist+'</td>'+'<td>'+statues[i].address+'</td>'+'<td><img src ='+statues[i].path+' height=100px width=100px></td></tr>';
         }
         htmlTable.concat('</table>');
         return htmlTable;
@@ -14,4 +15,4 @@ module.exports = {
     createHeader: function(data){
         
     }
-}
+};
