1aa6900d336eee3601e45dbf42c1920662a6a7ee
Tristan Canova <tcanova1@gmail.com>
2018-04-13 17:08:38 -0400

    implementing fuzzy search

--- statueController.js
+++ statueController.js
@@ -1,13 +1,21 @@
 var data = require('./statues.json');
 var jp = require('jsonpath');
+var Fuse = require('fuse.js');
+
+var options = {
+  keys: ['name']  
+};
 
 module.exports = {
     createTable: function(){
         var statues = jp.query(data,'$.*');
         htmlTable = '<link rel=stylesheet href=/css/style.css><table>';
         htmlTable += '<tr><th>Title:</th><th>Artist:</th><th>Location:</th><th>Picture:</th></tr>';
-        for(i in statues){
-            htmlTable += '<tr><td>'+statues[i].name+'</td>'+'<td>'+statues[i].artist+'</td>'+'<td>'+statues[i].address+'</td>'+'<td><img src ='+statues[i].path+' height=100px width=100px></td></tr>';
+        var fuzzySearch = new Fuse(statues,options);
+        stat = fuzzySearch.search('Lsoot');
+        //console.log(stat);
+        for(i in stat){
+            htmlTable += '<tr><td>'+stat[i].name+'</td>'+'<td>'+stat[i].artist+'</td>'+'<td>'+stat[i].address+'</td>'+'<td><img src ='+stat[i].path+' height=100px width=100px></td></tr>';
         }
         htmlTable.concat('</table>');
         return htmlTable;
