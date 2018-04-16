704ce6f110a05eda709e10f9e4eb2649f55ab8e4
Tristan Canova <tcanova1@gmail.com>
2018-04-16 07:27:07 -0400

    Finished up the project, commented the code, almost ready to send in! Just need to finish the readme

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 
+//Require the statueController.js file for creating our webpages when we need them
 var statues = require('./statueController.js');
 
 //Give the paths to the objects because they will not be static depending on where they are hosted
@@ -11,35 +12,30 @@ app.use('/js',express.static(__dirname + '/js'));
 app.use('/assets',express.static(__dirname + '/assets'));
 app.use('/node_modules',express.static(__dirname + '/node_modules'));
 
-//send the index.html as the root of the webpage
+//send the index.html as the root of the webpage, loads a collection view (or table) with a search bar to fuzzy search the database. More info in the index.html file itself
 app.get('/',function(req,res){
-    var webpage = statues.createHTMLBadly();
-    webpage += statues.createTable('');
-    webpage += statues.createAngular();
-    webpage += statues.finishHTMLBadly();
-    res.send(webpage);
-    //res.sendFile(__dirname+'/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
+//This is the beginning of the api endpoint, its not specialized so we show the whole collection view and have the search bar, sending just index.html like the root again.
 app.get('/data',function(req,res){
-    var webpage = statues.createHTMLBadly();
-    webpage += statues.createTable('');
-    webpage += statues.createAngular();
-    webpage += statues.finishHTMLBadly();
-    res.send(webpage);
+    res.sendFile(__dirname + '/index.html');
 });
 
+//This is the API endpoint, when hit it creates a webpage displaying the statue it found with the id. In the getSearch function we preform a fuzzy search so even if someone misspells the name of a statue they should get the correct result from this endpoint
 app.get('/data/:id',function(req,res){
-    var webpage = statues.createHTMLBadly();
-    webpage += statues.createTable(req.params.id);
-    webpage += statues.createAngular();
-    webpage += statues.finishHTMLBadly();
-    res.send(webpage);
+    var webpage = statues.createHTMLTop();//Create the top of the webpage
+    webpage += statues.getSearch(req.params.id);//Search and return the closest statue
+    webpage += statues.createHTMLBottom();//Close all of the html tags
+    res.send(webpage);//send the webpage
 });
 
-app.get('/picture',function(req,res){
-    res.sendFile(__dirname+'/photo.html');
+//This is the API endpoint that quiries the database for all data possible, used in index.html to show the collection view
+app.get('/test/allData',function(req,res){
+    var information = statues.getAll();
+    res.json(information);
 });
+
 
 //Make the server listen to port 8081
 server.listen(process.env.PORT || 8081,function(){
--- statueController.js
+++ statueController.js
@@ -1,43 +1,78 @@
+//This file acts as our "database" and has helpful methods for putting together a webpage when a person quiries for a specific statue or searches for a specific statue
+
+//Load the data, require jsonpath for quiries, require fuse.js for fuzzy search
 var data = require('./statues.json');
 var jp = require('jsonpath');
 var Fuse = require('fuse.js');
-//var angular = require('./node_modules/angular/angular.js');
+//var model = require('./statues.js');
 
+
+//Set up our options for the fuse fuzzy search, here we use thier default algorithm with no tuning and only search by name.
 var options = {
   keys: ['name']  
 };
 
+//Here are all of the functions that can be called by server.js to build a webpage based on the path or api endpoint that a user has entered
 module.exports = {
+    //This is an old method when I was testing things out and building a table the wrong way
     createTable: function(searchTerm){
         var statues = jp.query(data,'$.*');
-        htmlTable = '<table>';
+        var htmlTable = '<table>';
         htmlTable += '<tr><th>Title:</th><th>Artist:</th><th>Location:</th><th>Picture:</th></tr>';
         var fuzzySearch = new Fuse(statues,options);
         stat = statues;
         if(searchTerm == '')
         {
-            for(i in stat){
-                htmlTable += '<tr><td>'+stat[i].name+'</td>'+'<td>'+stat[i].artist+'</td>'+'<td>'+stat[i].address+'</td>'+'<td><img src ='+stat[i].path+' height=100px width=100px></td></tr>';
-            } 
+            htmlTable += model.createAllStatues(stat);
         }
         else
         {
             stat = fuzzySearch.search(searchTerm);
-            htmlTable += '<tr><td>'+stat[0].name+'</td>'+'<td>'+stat[0].artist+'</td>'+'<td>'+stat[0].address+'</td>'+'<td><img src ='+stat[0].path+' height=100px width=100px></td></tr>';
+            htmlTable += model.createOneStatue(stat[0]);
         }
         htmlTable.concat('</table>');
         return htmlTable;
     },
-    createHTMLBadly: function(){
-        badHTML = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Nipper API</title><link rel=stylesheet href=/css/style.css><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"><script src = "/node_modules/angular/angular.js"></script></head><body><div id="main">';
-        return badHTML;
+    //This function returns html that is at the top of the webpage, including everything from <!DOCTYPE html> linking all of the required scripts and stylesheets and ending with an open div with id=main.
+    createHTMLTop: function(){
+        var topHTML = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Nipper API</title><link rel=stylesheet href=/css/style.css><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"><script src = "/node_modules/angular/angular.js"></script></head><body><div id="main">';
+        return topHTML;
     },
-    createAngular: function(){
-        angularStuff = '<script> var nipperApp = angular.module("nipperApp",[]); nipperApp.controller("nipperController",function($scope){$scope.searchTerm = "";});</script><div ng-app= "nipperApp" ng-controller = "nipperController">Search: <input type="text" ng-model="searchTerm"><a ng-href = "/data/{{searchTerm}}" class="btn btn-default">Search</a></div>';
+    //This is the getAll function that is used when someone quiries for all of the data or the collection view/table is quiring for all of the data.
+    getAll: function(){
+        var information = jp.query(data,'$.*');
+        //console.log(information[0]);
+        return information;
+    },
+    //This is the getSearch method that is used to build a small webpage/api endpoint to display a specific statue or searched statue, it uses a fuzzysearch algorithm to try to find the closest match, if the searched name is so far off that fuzzysearch doesnt return any matches at all, an error page will appear alerting the user.
+    getSearch: function(searchTerm){
+        var img = '<div class="imgbox">';
+        var statues = jp.query(data,'$.*');//get all the data
+        var fuzzySearch = new Fuse(statues,options);//create a fuzzysearch object to preform the search
+        statues = fuzzySearch.search(searchTerm);//Actually do the search and fill the statues array with the best matches for what the user typed
+        var guessedStatue = statues[0];//Return only the best match
+        if(guessedStatue == undefined){//If someone typed in "zzzz" they would get an error message because no statues are closely named "zzzz"
+            img = '<div class="alert alert-danger"><Strong>Oops!</Strong> We are sorry! The name youve searched does not appear to come close enough for our searching algorithm to guess the statue you want! Please try searching with a different or modified name! </div>';
+            img += '<br> <a href="/data" class="btn btn-primary">Back</a>';
+            return img;
+        }
+        //Otherwise if fuzzysearch returns a result display that result and the information about the statue
+        img += '<img src="'+guessedStatue.path+'" alt="'+guessedStatue.name+'" class="center-fit" style="width:80%;">';
+        img += '<div class="text-block"><h4>'+guessedStatue.name+'</h4><p>By: '+guessedStatue.artist+'</p><br><p>Location: '+guessedStatue.address+'</div>';
+        img += '</div>';
+        //Always have a back button to bring them back to the collection view
+        img += '<br> <a href="/data" class="btn btn-primary">Back</a>';
+        return img;
+        
+    },
+    //Old method for creating the angular app without actually using HTML. NOT USED 
+    createAngularSearch: function(){
+        var angularStuff = '<script> var nipperApp = angular.module("nipperApp",[]); nipperApp.controller("nipperController",function($scope){$scope.searchTerm = "";});</script><div ng-app= "nipperApp" ng-controller = "nipperController">Search: <input type="text" ng-model="searchTerm"><a ng-href = "/data/{{searchTerm}}" class="btn btn-default">Search</a></div>';
         return angularStuff;
     },
-    finishHTMLBadly: function(){
-        finishedHTML = '</div></body></html>';
-        return finishedHTML;
+    //This method closes all of the tags opened by the createHTMLTop method.
+    createHTMLBottom: function(){
+        var bottomHTML = '</div></body></html>';
+        return bottomHTML;
     }
 };
